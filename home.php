fe786f12f9e3c1cc86f4fe3efd9d51ec23bcf874
fenilkanjiya <fenilkanjiya1611@gmail.com>
2020-09-08 21:15:13 +0530

    add jumbotron to home.php

--- home.php
+++ home.php
@@ -36,7 +36,7 @@
 
   <!-- Navbar End -->
 
-
+<div class="jumbotron">Welcome to Wobble </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
