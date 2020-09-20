6938b7aacff8401bc650bf7657c1603fe45037a8
fenilkanjiya <fenilkanjiya1611@gmail.com>
2020-09-20 12:50:36 +0530

    few chanes

--- php/home.php
+++ php/home.php
@@ -9,36 +9,16 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <link rel="stylesheet" type="text/css" href="../css/stlye.css" >
   </head>
   <body>
 	
-  <!-- Navbar start -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-primary sticky-top">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
+  
       <!-- logout -->
       <li class="nav-item float-right"><a class="nav-link" href  = "logout.php">Logout</a></li>
      
       <!-- logout end -->
-    
-    </ul>
-  </div>
-</nav>
-
-  <!-- Navbar End -->
+  
   <!-- search start -->
   <div class="container" >
   <div class="input-group col-md-5 my-3" style="margin:0 auto">
@@ -49,49 +29,38 @@
 </div>
  </div>
   <!-- search end -->
-  <!-- crousel start -->
-  <!-- <div class="container-fluid">
-  <div id="demo" class="carousel slide" data-ride="carousel" style="height:100%">
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./img/slider_image1.jpg" alt="Los Angeles" width="1500" height="50%" >
-      <div class="carousel-caption">
-        <h3>Los Angeles</h3>
-        <p>We had such a great time in LA!</p>
-      </div>   
-    </div>
-    <div class="carousel-item">
-      <img src="image2.jpg" alt="Chicago" width="1500" height="50%">
-      <div class="carousel-caption">
-        <h3>Chicago</h3>
-        <p>Thank you, Chicago!</p>
-      </div>   
-    </div>
-    <div class="carousel-item">
-      <img src="image3.jpg" alt="New York" width="1500" height="50%">
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
-      </div>   
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
-</div>
-  </div> -->
+  <!-- sidebar start -->
+  <div class="wrapper">
   
-  <!-- crousel end -->
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <h3>USER</h3>
+        </div>
 
-<div class="jumbotron">Welcome to Wobble </div>
+        <ul class="list-unstyled components">
+           
+            <li class="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" >Ragistration</a>
+            
+                
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" >Pages</a>
+                
+            </li>
+            
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+</div>
+<!-- sidebar end -->
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
