efee56f4b6fbb974c8ab3a489644be67af51b490
Himanshudabhi <himanshudabhi123@gmail.com>
2020-09-30 11:09:04 +0530

    change navbar admin,php

--- php/admin.php
+++ php/admin.php
@@ -14,7 +14,7 @@
     <h2>! WELCOME to Service Center !</h2>
 
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg ">
+    <!-- <nav class="navbar navbar-expand-lg ">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   </style>
     <span class="navbar-toggler-icon"></span>
@@ -44,7 +44,38 @@
       </li>
     </ul>
   </div>
+</nav> -->
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+
+  <a class="navbar-brand" href="#"><b>Admin</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="history.php">History</a>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link " href="#">Request</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+
 </nav>
+<div class="container">
+<p>jsdhgslrhg</p>
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- php/home.php
+++ php/home.php
@@ -79,7 +79,7 @@
  </div> -->
   <!-- search end-->
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
   <a class="navbar-brand" href="#"><b> User</b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
