ba71cde3495890d00fb2c3edfd514a6c27d0f24c
fenilkanjiya <fenilkanjiya1611@gmail.com>
2020-10-01 17:07:28 +0530

    few changes

--- php/index.php
+++ php/index.php
@@ -20,11 +20,7 @@
       <li class="nav-item">
         <a class="nav-link" href="#"><b>service center</b></a>
       </li>
-   
-     
     </ul>
-   
-
   </nav>
   <br>
   <!-- end navbar -->
@@ -35,10 +31,23 @@
     <div class="col-md-4">
       <img id="bannerid" src="../img/banner.png" class="img-fluid"></div>
       <div class="col-md-8">
-        <h2 id="h2dis">Wecome to <span class="wobblefont">service center</span></h2>
+        <h1 id="h2dis">Wecome to <span class="wobblefont"><b>service center</b               vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv></span></h1>
         <p>It has become a recurring debate as to which kind of service centre to choose for car servicing; an around-the-corner garage or an authorised service centres. While both have their pros and cons, below are some of the reasons why you should leave authorised service stations right now and never go back again.</p>
         <p>A car service, also known as a livery vehicle, is used to refer to a type of vehicle for hire transportation. It differs from yellow cab service in that the term "taxi" is used exclusively to refer to medallion taxis, which may be flagged down on the street on demand. By contrast, car service refers to services which are hired only by appointment over the telephone and dispatched by radio to the driver (taxicabs may also be hired in this way). Car service differs from the more upscale limousine services which are usually reserved for special occasions or business purposes</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequuntur veniam iste est ducimus magnam neque molestiae quisquam vitae distinctio animi laboriosam aspernatur iusto vel adipisci saepe eligendi, voluptatum quasi soluta ipsum velit nesciunt rerum dignissimos in. Fugit quos corrupti repudiandae odio ratione beatae tempora dicta consequatur commodi enim, veniam non inventore numquam exercitationem porro reiciendis ullam atque ipsam! Totam impedit repellat sequi, aut animi dolores ad maxime labore quae ipsam, maiores officiis tenetur? Ipsam velit quis ab expedita voluptatum porro fugiat atque provident temporibus! Nisi quo eaque cumque laborum consequatur provident culpa atque officia, ullam ducimus repellat laboriosam esse.</p>    
+        <p>Auto services is India’s leading chain of multi-brand car and bike service center
+        wide array of services. We focus on enhancing your uses experience by offering world-class
+        services.
+        Appliances maintenance services. Our sole mission is “To provide best Auto service in web
+        services to
+        keep the devices fit and healthy and customers happy and smiling”.
+
+        With well-equipped services Appliances service centres and fully trained mechanics, we
+        provide quality
+        services with excellent packages that are designed to offer you great savings.
+
+        Our state-of-art workshops are conveniently located in many cities across the country. Now you
+        can book
+        your service online by doing Registration.</p>    
     
         <a  class="btn btn-outline-secondary ml-auto" href="login.php" >Sign in</a>
         <a  class="btn btn-outline-secondary ml-auto" href="registration.php" >Sign up</a>
