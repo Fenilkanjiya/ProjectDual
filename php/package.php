37301c9ea8ac42fcc9880a45908baaadd1856c8c
Himanshudabhi <himanshudabhi123@gmail.com>
2020-10-06 17:46:41 +0530

    abc

--- php/package.php
+++ php/package.php
@@ -14,7 +14,6 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     
-  <link rel="stylesheet" type="text/css" href="../css/admin.css" >
   </head>
   <body>
 	
