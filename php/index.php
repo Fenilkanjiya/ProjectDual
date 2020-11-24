7b05fed32e8eb0b87c8e6557c1c92c6438b80dfd
Himanshudabhi <himanshudabhi123@gmail.com>
2020-11-24 14:30:43 +0530

    index.php change

--- php/index.php
+++ php/index.php
@@ -11,15 +11,39 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="../css/stlye.css" >
 </head>
+<style>
+  .logo{
+    width: 70px;
+    height: 65px;
+    
+  }
+  h3{
+    margin-left: 20px;
+    color: white;
+  }
+
+  body {
+    background-image: url(../img/background.jpg);
+    background-repeat: no-repeat;
+    height: 500px;
+    background-size: cover;
+    background-attachment: fixed;
+      }
+      p{
+        color: white;
+        background-color: red;
+      }
+  </style>
 
 <body>
 <!-- start navbar -->
 
-<nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color: #03DAC6;">
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top" >
     <ul class="navbar-nav ">
-      <li class="nav-item">
-        <a class="nav-link" href="#"><b>service center</b></a>
-      </li>
+     
+       
+        <img class="logo" src="../img/3.jpg" class="img-fluid"></div>
+      <h3>SERVICE CENTER</h3>
     </ul>
   </nav>
   <br>
@@ -28,11 +52,12 @@
 
   <div class="container-fluid">
     <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-4 mt-5">
       <img id="bannerid" src="../img/banner.png" class="img-fluid"></div>
       <!-- <header class="jumbotron back-image" style="background-image: url(../img/banner.png);"> -->
-      <div class="col-md-8">
-        <h1 id="h2dis">Wecome to <span class="wobblefont"><b>service center</b></span></h1>
+      <div class="col-md-8 mt-5">
+     
+        <h1 id="h2dis" style="color: white;">Wecome to <span class="wobblefont"><b>service center</b></span></h1>
         <p>It has become a recurring debate as to which kind of service centre to choose for car servicing; an around-the-corner garage or an authorised service centres. While both have their pros and cons, below are some of the reasons why you should leave authorised service stations right now and never go back again.</p>
         <p>A car service, also known as a livery vehicle, is used to refer to a type of vehicle for hire transportation. It differs from yellow cab service in that the term "taxi" is used exclusively to refer to medallion taxis, which may be flagged down on the street on demand. By contrast, car service refers to services which are hired only by appointment over the telephone and dispatched by radio to the driver (taxicabs may also be hired in this way). Car service differs from the more upscale limousine services which are usually reserved for special occasions or business purposes</p>
         <p>Auto services is India’s leading chain of multi-brand car and bike service center
@@ -50,8 +75,8 @@
         can book
         your service online by doing Registration.</p>    
     
-        <a  class="btn btn-outline-secondary ml-auto" href="login.php" >Sign in</a>
-        <a  class="btn btn-outline-secondary ml-auto" href="registration.php" >Sign up</a>
+        <a  class="btn btn-outline-secondary bg-primary text-white ml-auto" href="login.php" >Sign in</a>
+        <a  class="btn btn-outline-secondary bg-primary text-white ml-auto" href="registration.php" >Sign up</a>
       </div>
       </div>
      
